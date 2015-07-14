ee1fd3c0de44d83d82dac1a930201e0bd5aa274e
Ievgen Martynov <good.martynov@gmail.com>
2015-07-14 20:16:50 +0300

    fix for coffee-script dependency availability

--- setup.js
+++ setup.js
@@ -4,52 +4,49 @@ var fs = require('fs');
 
 var mode = process.argv[2];
 
-var fsExists = fs.exists || sysPath.exists;
+var fsExists = fs.exists;
+var fsExistsSync = fs.existsSync
 
 var execute = function(pathParts, params, callback) {
+  if (callback == null) callback = function() {};
   var path = sysPath.join.apply(null, pathParts);
   var command = 'node ' + path + ' ' + params;
-
-  if (callback == null) callback = function() {};
-  // npm install when run in parent package ignores the child's dependencies if such
-  // ones are already listed in parent's package.json:
-  // https://github.com/npm/npm/issues/2442
-  // https://github.com/npm/npm/issues/1341
-  // So, if parent app already depends on coffee-script  this package wont be installed
-  // resulting in 'missing module' error thrown since postinstall command considers
-  // the coffee bin be locally available
-  if (pathParts.slice(-1)[0] === 'coffee') {
-    executor(command, coffeeErrorHandler.bind(null, params))
-  } else {
-    executor(command);
-  }
-};
-
-var executor = function (command, callback) {
-  if (callback === undefined) {
-    callback = function(error, stdout, stderr) {
-      if (error != null) return process.stderr.write(stderr.toString());
-      console.log(stdout.toString());
-    };
-  }
   console.log('Executing', command);
-  exec(command, callback);
-};
-
-var coffeeErrorHandler = function (params, error, stdout, stderr) {
-  var command = 'coffee' + ' ' + params;
-  if (error != null) executor(command);
-  else {
+  exec(command, function(error, stdout, stderr) {
+    if (error != null) return process.stderr.write(stderr.toString());
     console.log(stdout.toString());
-  }
+  });
 };
 
 if (mode === 'postinstall') {
   fsExists(sysPath.join(__dirname, 'lib'), function(exists) {
     if (exists) return;
-    execute(['node_modules', 'coffee-script', 'bin', 'coffee'], '-o lib/ src/');
+    compileAndSave();
   });
 } else if (mode === 'test') {
   execute(['node_modules', 'mocha', 'bin', 'mocha'],
     '--require test/common.js --colors');
+}
+
+var compileAndSave = function () {
+  var coffee = require('coffee-script');
+  var srcDir = sysPath.join(__dirname, 'src');
+  var distDir = sysPath.join(__dirname, 'lib');
+  var filesToCompile = fs.readdirSync(srcDir);
+
+  if (!fsExistsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
+
+  filesToCompile.forEach(function (fileName) {
+    var filePath = sysPath.join(srcDir, fileName);
+    fs.readFile(filePath, {encoding: 'utf8'}, function(err, data) {
+      if (err) throw err;
+      var compiledJs = coffee.compile(data);
+      var distFileName = sysPath.join(distDir, fileName.replace(/coffee$/, 'js'));
+      fs.writeFile(distFileName, compiledJs, function (err) {
+        if (err) throw err;
+      });
+    });
+  });
 }
